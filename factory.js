39721e2049437f0189cc3c248b761554cf4d0ef6
Sunny Radadiya <radadiyasunny970@gmail.com>
2019-01-01 10:20:28 +0530

    Redeployed conract and added User interface to interact with smart contract

--- factory.js
+++ factory.js
@@ -2,6 +2,6 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x8755b5efE8343d5f0fb89d49f68B1EB8BBf0fe02'
+    '0xCce74DF0C6029de5A2b95A6DD0Ad646DFEdf2eF7'
 );
 export default instance;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../factory';
+import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component{
     static async getInitialProps(){
@@ -13,7 +15,7 @@ class CampaignIndex extends Component{
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaigns</a>,
+                description: (<Link route={`/campaigns/${address}`}><a>View Campaigns</a></Link>),                   
                 fluid: true
             };
         });
@@ -21,12 +23,26 @@ class CampaignIndex extends Component{
     }
 
     render(){
-        return <div>
-                    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"></link>
+        return (
+            <Layout>
+                <div>
                     <h3>Open Campaings</h3>
+
+                    <Link route='/campaigns/new'>
+                        <a>
+                            <Button 
+                                floated="right" 
+                                content="Create Campaign" 
+                                icon="add circle" 
+                                primary 
+                            />
+                        </a>
+                    </Link>
+                    
                     {this.renderCampaigns()}
-                    <Button content="Create Campaign" icon="add circle" primary />
-                </div>;
+                </div>
+            </Layout>
+        );
     }
 }
 export default CampaignIndex;
